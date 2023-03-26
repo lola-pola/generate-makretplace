c356c63f39e1b83004f054f2217d769c4461305e
Elhay Efrat <elhayefrat@elhays.local>
2023-03-26 12:14:07 +0300

    dding chat gpt
    Signed-off-by: Elhay Efrat <elhayefrat@elhays.local>

--- app/pages/chat_bot.py
+++ app/pages/chat_bot.py
@@ -4,32 +4,32 @@ import streamlit as st
 
 st.image('code.png',caption='qrcode',use_column_width="always")
 
-# def generate_gpt_chat(prompt):
-#     response = openai.Completion.create(
-#         engine="gpeta",
-#         prompt=prompt,
-#         temperature=1,
-#         max_tokens=8000
+def generate_gpt_chat(prompt):
+    response = openai.Completion.create(
+        engine="gpeta",
+        prompt=prompt,
+        temperature=0.5,
+        max_tokens=1024
         
         
-#     )
-#     print(response)
-#     return response.choices[0].text
+    )
+    print(response)
+    return response.choices[0].text
 
 
 
 
-# if 'generated' not in st.session_state:
-#     st.session_state['generated'] = []
-# if 'past' not in st.session_state:
-#     st.session_state['past'] = []
-# user_input=st.text_input("You:",key='input')
-# if user_input:
-#     output=generate_gpt_chat(user_input)
-#     #store the output
-#     st.session_state['past'].append(user_input)
-#     st.session_state['generated'].append(output)
-# if st.session_state['generated']:
-#     for i in range(len(st.session_state['generated'])-1, -1, -1):
-#         message(st.session_state["generated"][i], key=str(i))
-#         message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')     
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+if 'past' not in st.session_state:
+    st.session_state['past'] = []
+user_input=st.text_input("You:",key='input')
+if user_input:
+    output=generate_gpt_chat(user_input)
+    #store the output
+    st.session_state['past'].append(user_input)
+    st.session_state['generated'].append(output)
+if st.session_state['generated']:
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')     
