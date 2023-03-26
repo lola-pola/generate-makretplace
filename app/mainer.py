e77b9e07357e6bb01e16e8955d051261ab62e14c
Elhay Efrat <elhayefrat@elhays.local>
2023-03-26 09:49:48 +0300

    adding
    Signed-off-by: Elhay Efrat <elhayefrat@elhays.local>

--- app/mainer.py
+++ app/mainer.py
@@ -6,6 +6,8 @@ from daliegenerator import sending_picture ,sending_picture_small ,sending_pictu
 st.header("Hello WIX !!!")
 title = 'wix'
 
-texts = ['WIX icon beautiful realistic with snowflake super cool and shine ','WIX icon carton realistic with snowflake super cool and shine ','WIX icon fantasy realistic with snowflake super cool and shine ','WIX i]
+texts = ['WIX icon beautiful realistic with snowflake super cool and shine ',
+         'WIX icon carton realistic with snowflake super cool and shine ',
+         'WIX icon fantasy realistic with snowflake super cool and shine ']
 for _text in text:
     st.image(sending_picture_azure(_text),caption=title,use_column_width="always")
